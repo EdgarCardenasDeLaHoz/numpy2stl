4da45a5cfaa886898d53bc76bd18b1962953432b
EdgarCardenasDeLaHoz <35382761+EdgarCardenasDeLaHoz@users.noreply.github.com>
2020-03-14 20:44:35 -0400

    updated polygon simplification
    updated polygon simplification

--- numpy2stl.py
+++ numpy2stl.py
@@ -11,40 +11,65 @@ from shapely.geometry import Polygon, MultiPolygon
 from shapely.geometry.polygon import LinearRing, LineString, orient
 
 
-def numpy2stl(A, scale=0.1, mask_val=None, solid=False, min_thickness_percent=0.1 ):
+############################# convert array to facet list ##########################################
+
+def numpy2stl(A, mask_val=None, solid=False):
     """
-    Reads a numpy array, and outputs an STL file
+    Reads a numpy array, and list of facets
 
     Inputs:
      A (ndarray) - an 'm' by 'n' 2D numpy array
 
     Optional input:
-     scale (float)  - scales the height (surface) of the
-                    resulting STL mesh. Tune to match needs
 
      mask_val (float) - any element of the inputted array that is less than this value will not be included in the mesh.
                     default renders all vertices (x > -inf for all float x)
 
      solid (bool): sets whether to create a solid geometry (with sides and a bottom) or not.
-                    
-     min_thickness_percent (float) : when creating the solid bottom face, this
-                    multiplier sets the minimum thickness in the final geometry (shallowest interior
-                    point to bottom face), as a percentage of the thickness of the model computed up to
-                    that point.
-    Returns: (None)
+                                        
+    Returns: facets
     """
-
-    m, n = A.shape
-    #A = scale * (A - A.min())
-
     if mask_val is None:
         mask_val = A.min() - 1.
-
-    min_val = 0
+        
+    min_val = 0        
     
+    print("Creating top...")
+    top_facets = top_facet_from_array(A, mask_val = mask_val)
+
+    if solid:
+
+        #Calculate Floor z value
+        vertices = np.array([[facet[3:6],facet[6:9],facet[9:12]] for facet in top_facets])
+
+        print("Creating Edges...")
+        edges = get_open_edges(vertices)
+        edge_facets = facets_from_edges(edges, floor_val=min_val)
+
+        ##Bottom 
+        
+        print("Creating bottom...")
+
+        bottom_facets = []
+        for i, facet in enumerate(top_facets):
+
+            this_bottom = np.concatenate( [facet[:3], facet[6:8], [min_val], facet[3:5], [min_val], facet[9:11], [min_val]])
+            bottom_facets.append(this_bottom)
+
+        facets = np.concatenate([top_facets, edge_facets, bottom_facets])
+    
+    else:
+        facets = top_facets
+    
+    return facets
+
+
+def top_facet_from_array(A, mask_val=0, ):
+    
+    m, n = A.shape
     top_facets = []
     mask = np.zeros((m, n))
-    print("Creating top mesh...")
+
     for i, k in product(range(m - 1), range(n - 1)):
 
         this_pt = np.array([i, k, A[i, k]])
@@ -70,32 +95,8 @@ def numpy2stl(A, scale=0.1, mask_val=None, solid=False, min_thickness_percent=0.
             mask[i + 1, k] = 1
 
     top_facets = np.array(top_facets)
+    return top_facets
 
-    if solid:
-
-        print("Computing edges...")
-        #Calculate Floor z value
-        zvals = top_facets[:, 5::3]
-       
-        vertices = np.array([[facet[3:6],facet[6:9],facet[9:12]] for facet in top_facets])
-
-        edges = get_open_edges(vertices)
-        edge_facets = facets_from_edges(edges, floor_val=min_val)
-
-        ##Bottom 
-        bottom_facets = []
-        print("Creating bottom...")
-        for i, facet in enumerate(top_facets):
-
-            this_bottom = np.concatenate( [facet[:3], facet[6:8], [min_val], facet[3:5], [min_val], facet[9:11], [min_val]])
-            bottom_facets.append(this_bottom)
-
-        facets = np.concatenate([top_facets, edge_facets, bottom_facets])
-    
-    else:
-        facets = top_facets
-    
-    return facets
 
 def limit_facet_size(facets, max_width=1000., max_depth=1000., max_height=1000.):
     """
@@ -121,7 +122,7 @@ def facets_from_edges(edges, floor_val=0):
     """
     
     """ 
-    print("Creating Edges...")
+
     edge_facets = []
     n1,n2 = np.zeros(3),np.zeros(3)
 
@@ -144,7 +145,17 @@ def facets_from_edges(edges, floor_val=0):
 def roll2d(image, shifts):
     return np.roll(np.roll(image, shifts[0], axis=0), shifts[1], axis=1)
 
-####################### 3D Object analysis tools #######################################
+
+
+####################### 3D Object Processing tools #######################################
+
+def calculate_normals(vertices):
+    """
+    """
+    normals = np.cross(vertices[:,1] - vertices[:,0] , vertices[:,2] - vertices[:,0])
+    normals = normals / np.array([np.linalg.norm(normals, axis=1)]).T
+    
+    return normals
 
 def vertices_to_index(vertices):
     
@@ -159,6 +170,9 @@ def get_surfaces(vertices, normals=None):
         vertices is a M x 3 x 3 array of M triangles in 3D, 
         normals are [x,y,z] float normal vertices 
     """
+
+    ## Todo use shared edges not vertices to make surface 
+
     if normals is None:
         normals = calculate_normals(vertices)
     
@@ -167,8 +181,7 @@ def get_surfaces(vertices, normals=None):
     surfaces, surf = [],[0]
 
     b_checked = np.zeros(vertices_idx.shape[0],dtype=bool)    
-    vert = vertices_idx[0]
-    norm = normals[0] 
+    vert, norm  = vertices_idx[0], normals[0] 
     b_checked[0] = True
     to_do = []    
     
@@ -198,8 +211,7 @@ def get_surfaces(vertices, normals=None):
             idx = np.where(b_checked==False)[0][0]  
             
             surf.append(idx)
-            vert = vertices_idx[idx]
-            norm = normals[idx]
+            vert, norm  = vertices_idx[idx], normals[idx]
             b_checked[idx] = True 
             
             same_normal = np.array((norm==normals).all(axis=1))
@@ -208,102 +220,88 @@ def get_surfaces(vertices, normals=None):
     surfaces.append(list(set(surf)))
     return surfaces
 
-def simplify_surface(vertices,normal):
-    """
-    """
-    ## Get ordered perimeter points 
-    edges = get_open_edges(vertices)
-    perimeters = treverse_lines(edges)
-    
-    ## Flatten boundry to 2D
-    peri_points = [edges[peri,0] for peri in perimeters]
-    points2d = [ rotate_3D(points, normal, [0,0,1] ) for points in peri_points ]
-    
-    ## Simplify Triangulation 
-    vertices_tri = triangulate_polygon(points2d)
-    vertices_flat = vertices_tri.reshape(-1,vertices_tri.shape[-1])
-    
-    ## Reconstruct polygon from new triangles 
-    all_points_2d = np.concatenate(points2d)
-    idx_to_boundry = np.array([ np.nonzero(np.isclose(v, all_points_2d).all(axis=1))[0][0] for v in vertices_flat ])
-    idx_to_boundry = np.reshape(idx_to_boundry, vertices_tri.shape[0:2])
-    all_points = np.concatenate(peri_points)
-    
-    simplifed_vert = all_points[idx_to_boundry]
-    
-    return simplifed_vert
-
 def simplify_object_3D(vertices):
     """
     """
     surfaces = get_surfaces(vertices)
     normals = calculate_normals(vertices)
     
+
+    simplified_vertices = []
+    for surf in surfaces:
+        norm = normals[surf][0]
+        surface_vertices = vertices[surf]   
+
+        edges = get_open_edges(surface_vertices)
+        perimeters = get_ordered_perimeter(edges)
+        perimeter_2d = [rotate_3D(peri, norm, [0,0,1] ) for peri in perimeters ]
+        angles = [get_perimeter_angles(peri[:,0:2]) for peri in perimeter_2d]
+        points = [peri[angles[n] != 180] for n, peri in enumerate(perimeters)]
+        simplified_vertices.append(points)
+
+    keep_vertices = np.concatenate([np.concatenate(vert) for vert in simplified_vertices])   
+
     vertices_out = []
     for surf in surfaces:
         norm = normals[surf][0]
         surface_vertices = vertices[surf]   
+        
         if len(surface_vertices) > 1:
-            surface_vertices = simplify_surface(surface_vertices,norm) 
-            surface_vertices = validate_object(surface_vertices)
+            surface_vertices = simplify_surface(surface_vertices,norm, keep_vertices=keep_vertices) 
+            
         vertices_out.append( surface_vertices )  
+
+    vertices_out = np.concatenate(vertices_out)
+    vertices_out = validate_object(vertices_out)    
             
-    return np.concatenate(vertices_out)
+    return vertices_out
 
-def treverse_lines(lines_in):
+def simplify_surface(vertices,normal, keep_vertices=None):
     """
     """
-    starts_left = list(lines_in[:,0])
 
-    traces_all,trace = [],[]
-    
-    new_start = starts_left[0]
-    this_idx = np.where( (new_start==lines_in[:,0]).all(axis=1) )[0][0]
-    trace.append(this_idx)
-    
-    while len(starts_left)>0:
-        end_point = lines_in[this_idx,1]
-        next_idx_left = np.where((end_point==starts_left).all(axis=1))[0]
+    ## Get ordered perimeter points 
+    edges = get_open_edges(vertices)
+    perimeters = get_ordered_perimeter(edges)
 
-        if (len(next_idx_left) == 0):
-            traces_all.append(np.array(trace))
-            trace = []    
-            new_start = starts_left[0]
-            this_idx = np.where( (new_start==lines_in[:,0]).all(axis=1) )[0][0]
-        else:
-            starts_left.pop(next_idx_left[0])
-            next_idx = np.where( (end_point==lines_in[:,0]).all(axis=1) )[0][0]
-            this_idx = next_idx
-        trace.append(this_idx)
-            
-    traces_all.append(np.array(trace))
-    
-    #Validate the traces to be closed perimeters
-    traces_out = []
-    for trace in traces_all:
-        if (trace[0]==trace[-1]): 
-            traces_out.append(trace[0:-1])
-    return traces_out
+    if keep_vertices is not None:
+        perimeters = [peri[(peri[:,None] == keep_vertices).all(axis=2).any(axis=1)] for peri in perimeters]
 
-def calculate_normals(vertices):
-    """
-    """
-    normals = np.cross(vertices[:,1] - vertices[:,0] , vertices[:,2] - vertices[:,0])
-    normals = normals / np.array([np.linalg.norm(normals, axis=1)]).T
+    ## Flatten boundry to 2D
+    perimeter_2d = [rotate_3D(peri, normal, [0,0,1] ) for peri in perimeters ]
+    points_2d_flat = np.concatenate(perimeter_2d)
     
-    return normals
+    ## Simplify Triangulation 
+    vert_2D_tri = triangulate_polygon(perimeter_2d)
+    uni_vert_2D, idx_vert_2D = vertices_to_index(vert_2D_tri)
+
+    ## Reconstruct polygon from new triangles 
+    idx_to_boundry = np.array( [ np.nonzero(np.isclose(v, points_2d_flat).all(axis=1))[0][0] for v in uni_vert_2D ]   )
+    idx_to_boundry = idx_to_boundry[idx_vert_2D]
+    idx_to_boundry = np.reshape(idx_to_boundry, vert_2D_tri.shape[0:2])
+
+    all_points = np.concatenate(perimeters)
+    simplifed_vert = all_points[idx_to_boundry]
+
+    return simplifed_vert
 
 def validate_object(vertices):
     """
     """
+    ## Check for invalid triangles 
     normals = np.cross(vertices[:,1] - vertices[:,0] , vertices[:,2] - vertices[:,0])
     invalid = (normals==0).all(axis=1)
     vertices = vertices[invalid==False]
+
+    ## Check for invalid edges 
+    open_edges = get_open_edges(vertices)
+    if len(open_edges) > 0:
+        print("Open edges exist in object")
+
     return vertices
 
 def rotate_3D(pts, src_vertex, des_vertex):
     """
-    
     """
     mat = rotation_matrix_from_vertices(src_vertex, des_vertex)
     rotated_points = mat.dot(pts.T).T
@@ -323,10 +321,16 @@ def rotation_matrix_from_vertices(vec1, vec2):
     v = np.cross(a, b)
     c = np.dot(a, b)
     s = np.linalg.norm(v)
-    if s==0: s=1  
-        
     kmat = np.array([[0, -v[2], v[1]],  [v[2], 0, -v[0]],  [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+
+        
+    if s == 0 and c==1:
+        rotation_matrix =np.eye(3)
+    elif s == 0 and c==-1:
+        rotation_matrix = np.eye(3) * [1,-1,-1]
+    else:
+        rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+
     return rotation_matrix
 
 def triangulate_polygon(points):
@@ -335,47 +339,173 @@ def triangulate_polygon(points):
     """
     LR_ext = LinearRing(points[0])
     polygon = Polygon(LR_ext, points[1:])   
-    ## TODO: Make Triangulation Edge Constrained 
+    ##TODO: Make Triangulation Edge Constrained 
     tri_Poly = ops.triangulate(polygon)
+
     tri_Poly_within = [tri for tri in tri_Poly if tri.within(polygon)]
-    
+
     ## Check if Edge Case Convexity has occurred -- if the area out is not the area in
     intersect_area =  (ops.unary_union(tri_Poly_within)).area / polygon.area
     if np.isclose(intersect_area,1) == False:
         tri_Poly_outside = [tri for tri in tri_Poly if not tri.within(polygon)]    
-        if  len(tri_Poly_outside)>0:  
+        if  len(tri_Poly_outside)>0:      
             ## Union the remaining polygons and split them by the edge again
             Poly_outside = ops.unary_union(tri_Poly_outside)
             Poly_outside = ops.split(Poly_outside,LineString(points[0]))
+            Poly_outside = [ops.triangulate(tri) for tri in Poly_outside]
+            Poly_outside = [A for B in Poly_outside for A in B]
             tri_Poly_within.extend([orient(tri, sign=1) for tri in Poly_outside if tri.within(polygon)])
-            
+
     vert =  np.array([np.array(tri.exterior.xy)[:,0:3].T for tri in tri_Poly_within])
-    
+        
     all_points = np.concatenate(points)
     zval = np.full((*vert.shape[0:2],1), all_points[0,2] )  
     vertices_tri = np.concatenate((vert, zval ), axis =2)
-    
     return vertices_tri
+
 
 def get_open_edges(vertices):
     
     ## Make vertex list
-    vertices_list = np.reshape(vertices, (vertices.shape[0]*3,3))
-    uni_vertices, vert_idx = np.unique(vertices_list, axis=0, return_inverse=True)
-    vert_idx = np.reshape(vert_idx, (vertices.shape[0],3))
+    uni_vertices, vert_idx = vertices_to_index(vertices)
         
     ## Make Edges list
     edges = np.array([[[v[0],v[1]],[v[1],v[2]],[v[2],v[0]]] for v in vert_idx ])
     edge_list = np.reshape(edges, (edges.shape[0]*3, 2))
     
+    ## Count edge occurance 
     edge_sorted = np.sort(edge_list,axis=1)
     _, edge_idx, edge_counts = np.unique(edge_sorted, axis=0, return_counts=True, return_inverse=True)
     
     edge_counts_exp = edge_counts[edge_idx]
+    ## Select edges that only occur once and thus open
     open_edge_id = edge_list[edge_counts_exp==1]
+
+    ## Reconstruct Edge positions from locations
     open_edges = np.array(uni_vertices[open_edge_id])
     
-    return open_edges 
+    return open_edges
+
+
+def get_ordered_perimeter(edges,validate=False):
+    """
+    """
+    edges_list = np.reshape(edges, (-1,3))
+    uni_edges, edges_idx = np.unique(edges_list, axis=0 , return_inverse=True)
+    edges_idx = np.reshape(edges_idx, (edges.shape[0],-1))
+    
+    edges_left = list(edges_idx)
+    traces_all,trace = [],[]
+    this_idx = edges_left[0][0]
+    #while len(edges_left)>0:
+    #
+    for _ in range(len(edges_idx)):
+        trace.append(this_idx)  
+        list_idx = np.nonzero((edges_left == this_idx)[:,0])[0][0]
+
+        next_edge = edges_left.pop(list_idx)  
+        this_idx = next_edge[1]
+            
+        if np.any(this_idx==trace):
+            trace.append(this_idx)
+            traces_all.append(trace)
+            trace = []    
+            if len(edges_left)>0:
+                this_idx = edges_left[0][0]
+        
+    #Validate the traces to be closed perimeters
+    traces_out = []
+    for trace in traces_all:
+        if (trace[0]==trace[-1]): 
+            trace = trace[0:-1]    
+        traces_out.append(trace)
+
+    perimeters = [uni_edges[idx] for idx in traces_out]
+
+    return perimeters
+
+def get_perimeter_angles(peri):
+
+    peri_wrapped = np.concatenate([[peri[-1]],peri,[peri[0]]])
+
+    ba = peri_wrapped[0:-2] - peri_wrapped[1:-1]
+    bc = peri_wrapped[2::] - peri_wrapped[1:-1]
+    hypo = np.array(np.linalg.norm(ba, axis=1))* np.array(np.linalg.norm(bc, axis=1))
+
+    dot_prod = np.sum(ba*bc, axis=1) 
+    cross_prod = np.cross(bc,ba)
+    angle = np.arctan2(cross_prod,dot_prod)
+
+    angle = np.degrees(angle)
+    angle[angle<0] += 360
+    return angle 
+
+def partition_concave(perimeter):
+    angles = np.concatenate([get_perimeter_angles(p[:,0:2]) for p in perimeter])
+    convex_idx = np.nonzero( (angles == np.max(angles)) & (angles > 180)  )[0]
+
+    if len(convex_idx)==0:
+        return [perimeter]
+    
+    convex_idx = convex_idx[0]   
+    opposite_idx = find_closest_opposite(perimeter, convex_idx )
+     
+    if  convex_idx > opposite_idx:
+        idx1, idx2 = convex_idx, opposite_idx
+    else:
+        idx1, idx2 = opposite_idx, convex_idx
+
+    first_edge = np.concatenate(perimeter)[[idx1,idx2]]
+    last_edge =  np.concatenate(perimeter)[[idx2,idx1]]
+
+    edges = perimeters_to_edges(perimeter)
+    edges = np.concatenate([[first_edge],edges,[last_edge] ])
+    
+    peri_new = get_ordered_perimeter(edges)
+    peri_out = []
+    for p in peri_new:
+        peri_out.extend( partition_concave(peri_new))
+    return peri_out
+
+
+def find_closest_opposite(perimeter, idx):
+
+    perimeter_flat = np.concatenate(perimeter)
+    convex_point = perimeter_flat[idx]
+    bisect = get_bisect(perimeter_flat)[idx]
+    bisect = bisect / np.linalg.norm(bisect)
+    
+    other_idx = np.array([i for i in range(len(perimeter_flat)) if i != idx])
+
+    point_dist = perimeter_flat[other_idx] - convex_point
+    point_dist = point_dist / (np.linalg.norm(point_dist, axis=1))[:,None] 
+    opposite_dist = np.dot(point_dist,bisect)
+    opposite_idx = other_idx[np.nonzero(opposite_dist == np.max(opposite_dist))[0][0]]
+    return opposite_idx 
+
+def get_bisect(peri):
+
+    peri_wrapped = np.concatenate([[peri[-1]],peri,[peri[0]]])
+
+    ba = peri_wrapped[0:-2] - peri_wrapped[1:-1] 
+    ba = ba / np.array(np.linalg.norm(ba, axis=1))[:,None]
+    
+    bc = peri_wrapped[2::] - peri_wrapped[1:-1]
+    bc = bc / np.array(np.linalg.norm(bc, axis=1))[:,None]
+   
+    bisect = -(ba + bc)
+    #bisect = bisect / np.array(np.linalg.norm(bisect, axis=1))[:,None]
+    return bisect
+    
+def perimeters_to_edges(perimeters):
+
+    edges = np.concatenate([ np.array(list(zip(p, np.roll(p,1,axis=0) )))  for p in  perimeters ])
+
+    return edges 
+
+
+    
+
 
 ######################### Functions for Plotting in 3D ################################
 
@@ -454,8 +584,7 @@ def set_limits_3D(ax,x,y,z):
 
     ax.set_xlim(*xlim)
     ax.set_ylim(*ylim)
-    ax.set_zlim(*zlim)
-    
+    ax.set_zlim(*zlim)  
 
 ################### Write to file ##########################
 
@@ -489,7 +618,6 @@ def _build_ascii_stl(facets):
         lines.append(ASCII_FACET.format(face=facet))
     lines.append('endsolid ffd_geom')
     return lines
-
 
 def writeSTL(facets, file_name, ascii=False):
     """writes an ASCII or binary STL file"""
