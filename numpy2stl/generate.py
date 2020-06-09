3dc57f339574647982b72b22c719244c30eff333
EdgarCardenasDeLaHoz <Eac84.drexel@gmail.com>
2020-06-09 12:39:13 -0400

    Update generate.py
    remove redunant function: triangles_from_array

--- numpy2stl/generate.py
+++ numpy2stl/generate.py
@@ -28,7 +28,7 @@ def numpy2stl(A, mask_val=None, solid=False):
     min_val = 0        
     
     print("Creating top...")
-    top_vertices, top_faces = triangles_from_array(A, mask_val=mask_val)
+    top_vertices, top_faces = array2faces(A, mask_val=mask_val)
     top_triangles = top_vertices[top_faces]
 
     if solid:
@@ -36,13 +36,11 @@ def numpy2stl(A, mask_val=None, solid=False):
         ## Walls
         print("Creating walls...")
         edges = get_open_edges(top_faces)
-        
         perimeters = get_ordered_perimeter(top_vertices, edges )
         wall_triangles = perimeter_to_wall_vertices(top_vertices, perimeters, floor_val=min_val)
         
         ##Bottom 
         print("Creating bottom...")
-
         bottom_vertices = top_vertices
         bottom_vertices[:,2] = min_val
 
@@ -54,9 +52,7 @@ def numpy2stl(A, mask_val=None, solid=False):
     else:
         all_triangles = top_triangles
 
-    facets = triangles_to_facets(all_triangles)   
-
-    return facets
+    return all_triangles
 
 def array2faces(A, mask_val=0):
     
@@ -72,40 +68,11 @@ def array2faces(A, mask_val=0):
         if ((A[i, k] > mask_val) and (A[i, k+1] > mask_val) and 
             (A[i+1, k] > mask_val) and (A[i+1, k+1]  > mask_val)):
             
-            this_point = idxs[i, k]
-            top_right =  idxs[i, k+1]
-            bot_left  =  idxs[i+1, k]
-            bot_right =  idxs[i+1, k+1]    
-
-            faces.append( [top_right, this_point, bot_right] )
-            faces.append( [bot_right, this_point, bot_left] )
+            faces.append( [idxs[i, k+1], idxs[i, k], idxs[i+1, k+1]] )
+            faces.append( [idxs[i+1, k+1], idxs[i, k], idxs[i+1, k]] )
 
     faces = np.array(faces)
     return vertices, faces
-
-
-def triangles_from_array(A, mask_val=0, ):
-    
-    m, n = A.shape
-    facets = []
-    
-    for i, k in product(range(m - 1), range(n - 1)):
-
-        this_point = [i, k, A[i, k]]
-        top_right =  [i, k + 1, A[i, k + 1]]
-        bot_left  =  [i + 1, k , A[i + 1, k]]
-        bot_right =  [i + 1, k + 1, A[i + 1, k + 1]]
-
-        if ((this_point[-1] > mask_val) and (top_right[-1] > mask_val) and 
-            (bot_right[-1] > mask_val) and (bot_left[-1] > mask_val)):
-
-            facets.append( [top_right, this_point, bot_right] )
-            facets.append( [bot_right, this_point, bot_left] )
-
-    vertices = np.array(facets)
-    vertices, faces = vertices_to_index(vertices)
-    return vertices,faces
-
 
 def limit_facet_size(facets, max_width=1000., max_depth=1000., max_height=1000.):
     """
@@ -133,10 +100,9 @@ def perimeter_to_wall_vertices(vertices, perimeters, floor_val=0):
     wall_vertices = []
 
     for peri in perimeters: 
-
         peri = vertices[peri]
-
         peri_roll = np.roll(peri,1,axis=0)
+
         for n, point in enumerate(peri):
 
             top_left = np.concatenate([  point[0:2], [floor_val]  ])
