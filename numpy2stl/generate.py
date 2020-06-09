e8a8b0a188450a6fc6854e2ecd5975db0c4aecdc
EdgarCardenasDeLaHoz <Eac84.drexel@gmail.com>
2020-06-09 11:40:18 -0400

    Speed up triangle generation from Array
    Speed up triangle generation from Array

--- numpy2stl/generate.py
+++ numpy2stl/generate.py
@@ -24,73 +24,87 @@ def numpy2stl(A, mask_val=None, solid=False):
 
     if mask_val is None:
         mask_val = A.min() - 1.
-        
+
     min_val = 0        
     
     print("Creating top...")
-    top_facets = top_facet_from_array(A, mask_val = mask_val)
+    top_vertices, top_faces = triangles_from_array(A, mask_val=mask_val)
+    top_triangles = top_vertices[top_faces]
 
     if solid:
 
-        vertices = np.array([[facet[3:6],facet[6:9],facet[9:12]] for facet in top_facets])
-
         ## Walls
         print("Creating walls...")
-        edges = get_open_edges(vertices)
-        perimeter = get_ordered_perimeter(edges)
-
-        wall_verts = perimeter_to_wall_vertices(perimeter, floor_val=min_val)
-        wall_facets = vertices_to_facets(wall_verts)
-
+        edges = get_open_edges(top_faces)
+        
+        perimeters = get_ordered_perimeter(top_vertices, edges )
+        wall_triangles = perimeter_to_wall_vertices(top_vertices, perimeters, floor_val=min_val)
+        
         ##Bottom 
         print("Creating bottom...")
 
-        bottom_facets = []
-        for facet in top_facets:
-            this_bottom = np.concatenate( [facet[:3], facet[6:8], [min_val], facet[3:5], [min_val], facet[9:11], [min_val]])
-            bottom_facets.append(this_bottom)
+        bottom_vertices = top_vertices
+        bottom_vertices[:,2] = min_val
 
-            
-        facets = np.concatenate([top_facets, wall_facets, bottom_facets])
+        _, bottom_faces = simplify_surface(bottom_vertices, perimeters)
+        bottom_triangles = bottom_vertices[bottom_faces]
+        
+        all_triangles = np.concatenate([top_triangles, wall_triangles, bottom_triangles])
     
     else:
-        facets = top_facets
-    
+        all_triangles = top_triangles
+
+    facets = triangles_to_facets(all_triangles)   
+
     return facets
 
-
-def top_facet_from_array(A, mask_val=0, ):
+def array2faces(A, mask_val=0):
     
     m, n = A.shape
-    top_facets = []
-    mask = np.zeros((m, n))
+    xv,yv = np.meshgrid(range(m),range(n))
+    vertices = np.stack([xv.ravel(),yv.ravel(),A.ravel()]).T
 
+    idxs = np.array(range(m*n)).reshape(m,n)
+
+    faces = []
     for i, k in product(range(m - 1), range(n - 1)):
 
-        this_pt = np.array([i, k, A[i, k]])
-        top_right = np.array([i, k + 1, A[i, k + 1]])
-        bottom_left = np.array([i + 1, k , A[i + 1, k]])
-        bottom_right = np.array([i + 1, k + 1, A[i + 1, k + 1]])
+        if ((A[i, k] > mask_val) and (A[i, k+1] > mask_val) and 
+            (A[i+1, k] > mask_val) and (A[i+1, k+1]  > mask_val)):
+            
+            this_point = idxs[i, k]
+            top_right =  idxs[i, k+1]
+            bot_left  =  idxs[i+1, k]
+            bot_right =  idxs[i+1, k+1]    
 
-        n1, n2 = np.zeros(3), np.zeros(3)
+            faces.append( [top_right, this_point, bot_right] )
+            faces.append( [bot_right, this_point, bot_left] )
 
-        if ((this_pt[-1] > mask_val) and (top_right[-1] > mask_val) and 
-                (bottom_right[-1] > mask_val) and (bottom_left[-1] > mask_val)):
+    faces = np.array(faces)
+    return vertices, faces
 
-            facet = np.concatenate([n1, top_right, this_pt, bottom_right])
-            mask[i, k] = 1
-            mask[i, k + 1] = 1
-            mask[i + 1, k] = 1
-            top_facets.append(facet)
 
-            facet = np.concatenate([n2, bottom_right, this_pt, bottom_left])
-            top_facets.append(facet)
-            mask[i, k] = 1
-            mask[i + 1, k + 1] = 1
-            mask[i + 1, k] = 1
+def triangles_from_array(A, mask_val=0, ):
+    
+    m, n = A.shape
+    facets = []
+    
+    for i, k in product(range(m - 1), range(n - 1)):
 
-    top_facets = np.array(top_facets)
-    return top_facets
+        this_point = [i, k, A[i, k]]
+        top_right =  [i, k + 1, A[i, k + 1]]
+        bot_left  =  [i + 1, k , A[i + 1, k]]
+        bot_right =  [i + 1, k + 1, A[i + 1, k + 1]]
+
+        if ((this_point[-1] > mask_val) and (top_right[-1] > mask_val) and 
+            (bot_right[-1] > mask_val) and (bot_left[-1] > mask_val)):
+
+            facets.append( [top_right, this_point, bot_right] )
+            facets.append( [bot_right, this_point, bot_left] )
+
+    vertices = np.array(facets)
+    vertices, faces = vertices_to_index(vertices)
+    return vertices,faces
 
 
 def limit_facet_size(facets, max_width=1000., max_depth=1000., max_height=1000.):
@@ -112,13 +126,15 @@ def limit_facet_size(facets, max_width=1000., max_depth=1000., max_height=1000.)
 
     return facets
 
-def perimeter_to_wall_vertices(perimeters, floor_val=0):
+def perimeter_to_wall_vertices(vertices, perimeters, floor_val=0):
     """
     
     """ 
     wall_vertices = []
 
     for peri in perimeters: 
+
+        peri = vertices[peri]
 
         peri_roll = np.roll(peri,1,axis=0)
         for n, point in enumerate(peri):
@@ -142,9 +158,9 @@ def roll2d(image, shifts):
 
     return np.roll(np.roll(image, shifts[0], axis=0), shifts[1], axis=1)
 
-def vertices_to_facets(vertices):
+def triangles_to_facets(triangles):
 
-    normals = calculate_normals(vertices)
-    facets = np.array([np.concatenate([normals[n],v[0],v[1],v[2]]) for n,v in enumerate(vertices)])
+    normals = calculate_normals(triangles)
+    facets = np.array([np.concatenate([normals[n],v[0],v[1],v[2]]) for n,v in enumerate(triangles)])
 
     return facets
