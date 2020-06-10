338a89c09f597b06af0e6a2237b47e616338eb37
EdgarCardenasDeLaHoz <Eac84.drexel@gmail.com>
2020-06-09 22:33:33 -0400

    clean solid.py and methods

--- numpy2stl/solid.py
+++ numpy2stl/solid.py
@@ -13,6 +13,14 @@ class Solid:
         self.surfaces = surfaces
         self.normals = normals 
 
+    
+    def validate_object(self):
+        validate_object(self)
+
+    def simplify(self):
+        simplify_object_3D(self)
+
+
         
 def calculate_normals(triangles):
     """
@@ -54,7 +62,7 @@ def get_surfaces(triangles, normals=None):
     _, edge_idx = index_edges(triangles)
 
     surfaces = []
-    surf_normals = []
+    
     # for each set of coplanear facets 
     for n_idx in range(len(uni_normals)):
 
@@ -76,11 +84,11 @@ def get_surfaces(triangles, normals=None):
 
 def contiguous_edges(edge_idx, idx_list):
 
-    b_checked = np.zeros(edge_idx.shape[0],dtype=bool)  
+    b_unchecked = np.ones(edge_idx.shape[0],dtype=bool)  
     surfaces, surf, queue = [],[],[]
 
     sub_idx = 0
-    b_checked[sub_idx] = True
+    b_unchecked[sub_idx] = False
     idx = idx_list[sub_idx]
     surf.append(idx) 
 
@@ -94,11 +102,11 @@ def contiguous_edges(edge_idx, idx_list):
         edges  = edge_idx[ sub_idx ]
         for e in edges:
             for idx in edge_dict[e]:
-                if idx != sub_idx and b_checked[idx]==False:
+                if idx != sub_idx and b_unchecked[idx]:
                     
                     queue.append(idx)
                     surf.append(idx_list[idx])   
-                    b_checked[idx] = True
+                    b_unchecked[idx] = False
 
         #Continue treversing touching vertices or in queue
         if len(queue)>0:
@@ -108,62 +116,70 @@ def contiguous_edges(edge_idx, idx_list):
         else:
             surfaces.append(np.array(surf))
             surf = []
-            sub_idx = np.where(b_checked==False)[0][0]  
+
+            sub_idx = np.where(b_unchecked)[0][0]
+
             surf.append( idx_list[sub_idx] )
-            b_checked[sub_idx] = True
+            b_unchecked[sub_idx] = False
                  
     surfaces.append(np.array(surf))
-
     return surfaces
 
 def simplify_object_3D(solid):
     """
     """
+    vertices = solid.vertices
     normals = solid.normals
+    surfaces = solid.surfaces
+    faces = solid.faces
+
     perimeter_list = get_perimeter_list(solid)
 
-    min_vertices = get_min_required_vertices( perimeter_list, normals)
+    min_vert_idx = get_min_required_vertices( vertices, perimeter_list, normals)
+    faces_out = []
 
-    vertices_out = []
     for n, peri in enumerate(perimeter_list):
         
         if len(peri) == 1 and len(peri[0])==3:
-            surface_vertices = peri[0][None,:]
+
+            new_faces = [peri[0]]
+
+        elif len(peri) == 1 and len(peri[0])==4:
+
+            new_faces = faces[surfaces[n]]
+            
         else:
             norm = normals[n]
+            min_vert_idx
+            peri = [p for p in peri]
+            _, new_faces = simplify_surface( vertices, peri, norm, ) 
 
-            peri = [p[(p[:,None] == [min_vertices]).all(axis=2).any(axis=1)] for p in peri]
+        faces_out.append( new_faces )  
+    facets_out = vertices[np.concatenate(faces_out)]
 
-            surface_vertices = simplify_surface( peri, norm, ) 
-
-        vertices_out.append( surface_vertices )  
-    vertices_out = np.concatenate(vertices_out)
-
-    solid = Solid(vertices_out)
-
-    #solid = validate_object(solid)    
+    solid = Solid(facets_out)
+    validate_object(solid)   
     return solid
 
-
-def simplify_surface(perimeters, normal ):
+def simplify_surface(vertices, perimeters, normal=None):
     """
     """    
     ## Flatten boundry to 2D
-    perimeter_2d = perimeter_to_2D( perimeters, normal, simplify_lines=False)
-    
-    _,faces = triangulate_polygon( perimeter_2d )
+    if normal is None:
+        normal = np.array([0,0,1])
 
-    simplifed_vert = np.concatenate(perimeters)[faces]
+    sub_verts = vertices[np.concatenate(perimeters)]
+    sub_peri = []
+    end = 0 
+    for p in perimeters:
+        sub_peri.append(np.arange(len(p))+end)
+        end += len(p)
 
-    #sum1 = np.sum(np.array([get_area(p) if n==0 else -get_area(p) for n,p in enumerate(perimeters)]))
-    #sum2 = np.sum(np.array([get_area(v) for v in simplifed_vert]))
+    sub_verts_2D = vertices_to_2D( sub_verts, normal)
+    _,sub_faces = triangulate_polygon( sub_verts_2D , sub_peri)
+    faces = np.concatenate(perimeters)[sub_faces]
 
-    #if np.isclose(sum1, sum2) == False:
-        
-    #    print(  '1', sum1 )
-    #    print(  '2', sum2 )
-        
-    return simplifed_vert
+    return sub_verts, faces
 
 def get_perimeter_list(solid):
 
@@ -174,36 +190,38 @@ def get_perimeter_list(solid):
     perimeter_list = []
     for surf in surfaces:
 
-        surface_vertices = vertices[faces[surf]]
-        
-        if len(surface_vertices)==1 and len(surface_vertices[0]):
-            perimeter_list.append([surface_vertices[0]])  
+        surf_faces = faces[surf]
+        if len(surf)<2:
+
+            perimeter_list.append([surf_faces[0]]) 
             continue
 
-        edges = get_open_edges(surface_vertices)
-        perimeters = get_ordered_perimeter(edges)
+        edges = get_open_edges(surf_faces)
+        perimeters = get_ordered_perimeter(vertices, edges)
         
-        perimeter_list.append(perimeters)   
+        perimeter_list.append(perimeters)
 
     return perimeter_list
 
 
-def get_min_required_vertices( perimeter_list, normal_list):
+def get_min_required_vertices( vertices, perimeter_list, normal_list):
 
-    simplified_vertices = []  
+    simplified_vertices = []
 
     for n, perimeters in enumerate(perimeter_list):
 
         norm = normal_list[n]
-        simplified_perimeters = simplify_perimeters( perimeters, norm)
-        if len(simplified_perimeters)==0:          continue
+        simplified_perimeters = simplify_perimeters( vertices, perimeters, norm)
+
+        if len(simplified_perimeters)==0:
+            continue
             
         simplified_vertices.append(  np.concatenate( simplified_perimeters )  )
 
-    required_vertices = np.concatenate(simplified_vertices)  
-    required_vertices = np.unique(required_vertices, axis=0)
+    req_vert_idx = np.concatenate(simplified_vertices)  
+    req_vert_idx = np.unique(req_vert_idx, axis=0)
 
-    return required_vertices
+    return req_vert_idx
 
     
 def validate_object(solid):
@@ -225,14 +243,16 @@ def validate_object(solid):
         print("invalid faces exist in object!!")
 
     ## Check for invalid edges 
-    open_edges = get_open_edges(triangles)
+    open_edges = get_open_edges(faces)
     if len(open_edges) > 0:
 
         is_valid = False
         print(open_edges)
         print("Open edges exist in object!!")
 
-    return is_valid
+    if (is_valid)==False:
+        print("Solid is not valid")
+
 
 def index_edges(vertices):
 
@@ -249,12 +269,10 @@ def index_edges(vertices):
 
     return uni_edges, edge_idx
 
-def get_open_edges(vertices):
-    
-    ## Make vertex list
-    uni_vertices, vert_idx = vertices_to_index(vertices)
+def get_open_edges(faces):
+        
     ## Make Edges list
-    edges = vert_idx[:,[[0,1],[1,2],[2,0]]]
+    edges = faces[:,[[0,1],[1,2],[2,0]]]
     edge_list = np.reshape(edges, (edges.shape[0]*3, 2))
     ## Count edge occurance 
     edge_sorted = np.sort(edge_list,axis=1)
@@ -262,9 +280,8 @@ def get_open_edges(vertices):
 
     edge_counts_exp = edge_counts[edge_idx]
     ## Select edges that only occur once and thus open
-    open_edge_id = edge_list[edge_counts_exp==1]
+    open_edges = edge_list[edge_counts_exp==1]
     ## Reconstruct Edge positions from locations
-    open_edges = np.array(uni_vertices[open_edge_id])
     
     return open_edges
 
